67bdfe723ba560a9c4cf5d0fa90f0ac8574e8674
Lepaz <arteleandropaz@gmail.com>
2026-09-08 22:15:53 -0300

    08/09/26_Arrumar_PostgreSQL_Database/Eventspace
    08/09/26 Arrumar Controller/SalaController, Entities/Sala/Usuario, criar DTO/AtualizaStatusSalaRequest.

--- src/main/java/com/example/aulajovem2026ia/controllers/SalaController.java
+++ src/main/java/com/example/aulajovem2026ia/controllers/SalaController.java
@@ -1,6 +1,9 @@
 package com.example.aulajovem2026ia.controllers;
 
+import com.example.aulajovem2026ia.DTO.AtualizaStatusSalaRequest;
+import com.example.aulajovem2026ia.DTO.AtualizaStatusUsuarioRequest;
 import com.example.aulajovem2026ia.DTO.SalaResponse;
+import com.example.aulajovem2026ia.DTO.UsuarioResponse;
 import com.example.aulajovem2026ia.entities.Sala;
 import com.example.aulajovem2026ia.entities.Usuario;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +20,10 @@ public class SalaController {
     @GetMapping("/{id}")
     public Sala ConsultaSalaPorID(@PathVariable Long id) {
         Sala sala = new Sala();
-        Sala.setNome("Leandro");
-        Sala.setNumero("0213154545");
-        Sala.setAndar("03");
-        Sala.setClasse("B");
+        sala.setNome("Leandro");
+        sala.setNumero("0213154545");
+        sala.setAndar("03");
+        sala.setClasse("B");
         return sala;
     }
 
@@ -38,6 +41,7 @@ public class SalaController {
         salaBanco.setNumero(salaRequest.getNumero());
         salaBanco.setAndar(salaRequest.getAndar());
         salaBanco.setClasse(salaRequest.getClasse());
+        salaBanco.setStatus("A");
 
         return ResponseEntity.ok(new SalaResponse(salaBanco.getId(),
                 "Sala Atualizado com sucesso!"));
@@ -64,4 +68,35 @@ public class SalaController {
         return ResponseEntity.notFound().build();
 
     }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<SalaResponse> AtualizarSala(@PathVariable Long id, @RequestBody AtualizaStatusSalaRequest salaRequest) {
+
+        //Consuta no banco
+        Sala salaBanco = new Sala();
+
+        if (salaBanco !=null){
+            salaBanco.setStatus(salaBanco.getStatus());
+
+            return ResponseEntity.ok(new SalaResponse(salaBanco.getId(),
+                    "Usuario Atualizado com sucesso!"));
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<SalaResponse> AtualizarStatus(@PathVariable Long id){
+        //Consuta no banco
+        Sala salaBanco = new Sala();
+
+        if (salaBanco !=null){
+            salaBanco.setStatus("D");
+
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 } //fim da minha classe.

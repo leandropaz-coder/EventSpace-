48693b7abfe6a47ee1930ed5d3d2c3631003ec79
Lepaz <arteleandropaz@gmail.com>
2026-09-13 21:00:26 -0300

    13/09/2026 revisão dos codigos IJ
    Conforme a AULA5,feito a revisão Controllers/usuario,cliente_ DTO/usuario,cliente_ Entities/usuario,cliente_Repository/usuario,cliente

--- src/main/java/com/example/aulajovem2026ia/DTO/AtualizaStatusSalaRequest.java
+++ src/main/java/com/example/aulajovem2026ia/DTO/AtualizaStatusSalaRequest.java
@@ -1,4 +1,14 @@
 package com.example.aulajovem2026ia.DTO;
 
 public class AtualizaStatusSalaRequest {
-}
+
+    public AtualizaStatusSalaRequest() {
+    }
+
+    public String status;
+
+    public String getStatus() {return status;}
+
+    public void setStatus(String status) {this.status = status;}
+
+} //fim da minha classe.
